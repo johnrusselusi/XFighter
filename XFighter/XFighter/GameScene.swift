e8efab2143819989cc15775442f3790a79a12806
John Russel Usi <j.usi@klab.com>
2015-10-29 12:06:02 +0800

    Project setup.

---
+++ XFighter/XFighter/GameScene.swift
@@ -0,0 +1,14 @@
+//
+//  GameScene.swift
+//  XFighter
+//
+//  Created by John Russel Usi on 10/29/15.
+//  Copyright (c) 2015 CYS. All rights reserved.
+//
+
+import SpriteKit
+
+class GameScene: SKScene
+{
+    
+}
